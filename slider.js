fe098e07f00cd2080048b9f9d601bd9f42a334ad
Elaine Malone <xomisse@users.noreply.github.com>
2016-10-10 17:44:56 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,61 +1,222 @@
-var showpostthumbnails_gal  = true;
-    var showpostsummary_gal  	= true;
-	var random_posts       		= false;
-	var numchars_gal			= 100;
-	var numposts_gal			= 10;
-function showgalleryposts(json) {
-    var numPosts = json.feed.openSearch$totalResults.$t;
-    var indexPosts = new Array();
-	document.write('<ul>');
-    for (var i = 0; i < numPosts; ++i) {
-        indexPosts[i] = i;
-    }
-	if (random_posts == true){
-    indexPosts.sort(function() {return 0.5 - Math.random()});
-	}
-    if (numposts_gal > numPosts) {
-        numposts_gal = numPosts;
-    }
-    for (i = 0; i < numposts_gal; ++i) {
-        var entry_gal = json.feed.entry[indexPosts[i]];
-		var posttitle_gal =  entry_gal.title.$t;
-        for (var k = 0; k <  entry_gal.link.length; k++) {
-            if ( entry_gal.link[k].rel == 'alternate') {
-                posturl_gal =  entry_gal.link[k].href;
-                break;
-            }
-        }
-		if ("content" in entry_gal) {
-            var postcontent_gal = entry_gal.content.$t
-        }
-        s = postcontent_gal;
-        a = s.indexOf("<img");
-        b = s.indexOf("src=\"", a);
-        c = s.indexOf("\"", b + 5);
-        d = s.substr(b + 5, c - b - 5);
-        if ((a != -1) && (b != -1) && (c != -1) && (d != "")) {
-            var thumburl_gal = d
-        } else var thumburl_gal = 'http://3.bp.blogspot.com/-KlqxInoaLtw/VImV4tYhDjI/AAAAAAAABCc/IRQ8PQ4cVro/s1600/no-img.jpg';
-        document.write('<li><div id="slide-container"><span class="slide-desc"><h2 style="margin:10px 0px;">');
-        document.write(posttitle_gal + '</h2>');
-        var re = /<\S[^>]*>/g;
-        postcontent_gal = postcontent_gal.replace(re, "");
-        if (showpostsummary_gal == true) {
-            if (postcontent_gal.length < numchars_gal) {
-                document.write(postcontent_gal);
-                document.write('</span>')
+/*
+ * 	Easy Slider 1.7 - jQuery plugin
+ *	written by Alen Grakalic	
+ *	http://cssglobe.com/post/4004/easy-slider-15-the-easiest-jquery-plugin-for-sliding
+ *
+ *	Copyright (c) 2009 Alen Grakalic (http://cssglobe.com)
+ *	Dual licensed under the MIT (MIT-LICENSE.txt)
+ *	and GPL (GPL-LICENSE.txt) licenses.
+ *
+ *	Built for jQuery library
+ *	http://jquery.com
+ *
+ */
+ 
+/*
+ *	markup example for $("#slider").easySlider();
+ *	
+ * 	<div id="slider">
+ *		<ul>
+ *			<li><img src="images/01.jpg" alt="" /></li>
+ *			<li><img src="images/02.jpg" alt="" /></li>
+ *			<li><img src="images/03.jpg" alt="" /></li>
+ *			<li><img src="images/04.jpg" alt="" /></li>
+ *			<li><img src="images/05.jpg" alt="" /></li>
+ *		</ul>
+ *	</div>
+ *
+ */
+
+ 
+(function ($) {
+
+    $.fn.easySlider = function (options) {
+
+        // default configuration properties
+        var defaults = {
+            prevId: 'prevBtn',
+            prevText: 'Previous',
+            nextId: 'nextBtn',
+            nextText: 'Next',
+            controlsShow: true,
+            controlsBefore: '',
+            controlsAfter: '',
+            controlsFade: true,
+            firstId: 'firstBtn',
+            firstText: 'First',
+            firstShow: false,
+            lastId: 'lastBtn',
+            lastText: 'Last',
+            lastShow: false,
+            vertical: false,
+            speed: 800,
+            auto: false,
+            pause: 2000,
+            continuous: false,
+            numeric: false,
+            numericId: 'controls'
+        };
+
+        var options = $.extend(defaults, options);
+
+        this.each(function () {
+            var obj = $(this);
+            var s = $("li", obj).length;
+            var w = $("li", obj).width();
+            var h = $("li", obj).height();
+            var clickable = true;
+            obj.width(w);
+            obj.height(h);
+            obj.css("overflow", "hidden");
+            var ts = s - 1;
+            var t = 0;
+            $("ul", obj).css('width', s * w);
+
+            if (options.continuous) {
+                $("ul", obj).prepend($("ul li:last-child", obj).clone().css("margin-left", "-" + w + "px"));
+                $("ul", obj).append($("ul li:nth-child(2)", obj).clone());
+                $("ul", obj).css('width', (s + 1) * w);
+            };
+
+            if (!options.vertical) $("li", obj).css('float', 'left');
+
+            if (options.controlsShow) {
+                var html = options.controlsBefore;
+                if (options.numeric) {
+                    html += '<ol id="' + options.numericId + '"></ol>';
+                } else {
+                };
+
+                html += options.controlsAfter;
+                $(obj).after(html);
+            };
+
+            if (options.numeric) {
+                for (var i = 0; i < s; i++) {
+                    $(document.createElement("li"))
+						.attr('id', options.numericId + (i + 1))
+						.html('<a rel=' + i + ' href=\"javascript:void(0);\">' + (i + 1) + '</a>')
+						.appendTo($("#" + options.numericId))
+						.click(function () {
+						    animate($("a", $(this)).attr('rel'), true);
+						});
+                };
             } else {
-                postcontent_gal = postcontent_gal.substring(0, numchars_gal);
-                var quoteEnd_gal = postcontent_gal.lastIndexOf(" ");
-                postcontent_gal = postcontent_gal.substring(0, quoteEnd_gal);
-                document.write(postcontent_gal + '');
-                document.write('</span>')
-            }
-        }
-		 document.write('<a href="' + posturl_gal + '"><img src="' + thumburl_gal + '" width="100%" height="auto"/></a></div>');
-        document.write('</li>');
-    }
-	document.write('</ul>');
-	
-}
-var numposts_gal = 10; var numchars_gal = 0; var random_posts = false; 
+                $("a", "#" + options.nextId).click(function () {
+                    animate("next", true);
+                });
+                $("a", "#" + options.prevId).click(function () {
+                    animate("prev", true);
+                });
+                $("a", "#" + options.firstId).click(function () {
+                    animate("first", true);
+                });
+                $("a", "#" + options.lastId).click(function () {
+                    animate("last", true);
+                });
+            };
+
+            function setCurrent(i) {
+                i = parseInt(i) + 1;
+                $("li", "#" + options.numericId).removeClass("current");
+                $("li#" + options.numericId + i).addClass("current");
+            };
+
+            function adjust() {
+                if (t > ts) t = 0;
+                if (t < 0) t = ts;
+                if (!options.vertical) {
+                    $("ul", obj).css("margin-left", (t * w * -1));
+                } else {
+                    $("ul", obj).css("margin-left", (t * h * -1));
+                }
+                clickable = true;
+                if (options.numeric) setCurrent(t);
+            };
+
+            function animate(dir, clicked) {
+                if (clickable) {
+                    clickable = false;
+                    var ot = t;
+                    switch (dir) {
+                        case "next":
+                            t = (ot >= ts) ? (options.continuous ? t + 1 : ts) : t + 1;
+                            break;
+                        case "prev":
+                            t = (t <= 0) ? (options.continuous ? t - 1 : 0) : t - 1;
+                            break;
+                        case "first":
+                            t = 0;
+                            break;
+                        case "last":
+                            t = ts;
+                            break;
+                        default:
+                            t = dir;
+                            break;
+                    };
+                    var diff = Math.abs(ot - t);
+                    var speed = diff * options.speed;
+                    if (!options.vertical) {
+                        p = (t * w * -1);
+                        $("ul", obj).animate(
+							{ marginLeft: p },
+							{ queue: false, duration: speed, complete: adjust }
+						);
+                    } else {
+                        p = (t * h * -1);
+                        $("ul", obj).animate(
+							{ marginTop: p },
+							{ queue: false, duration: speed, complete: adjust }
+						);
+                    };
+
+                    if (!options.continuous && options.controlsFade) {
+                        if (t == ts) {
+                            $("a", "#" + options.nextId).hide();
+                            $("a", "#" + options.lastId).hide();
+                        } else {
+                            $("a", "#" + options.nextId).show();
+                            $("a", "#" + options.lastId).show();
+                        };
+                        if (t == 0) {
+                            $("a", "#" + options.prevId).hide();
+                            $("a", "#" + options.firstId).hide();
+                        } else {
+                            $("a", "#" + options.prevId).show();
+                            $("a", "#" + options.firstId).show();
+                        };
+                    };
+
+                    if (clicked) clearTimeout(timeout);
+                    if (options.auto && dir == "next" && !clicked) {
+                        ;
+                        timeout = setTimeout(function () {
+                            animate("next", false);
+                        }, diff * options.speed + options.pause);
+                    };
+
+                };
+
+            };
+            // init
+            var timeout;
+            if (options.auto) {
+                ;
+                timeout = setTimeout(function () {
+                    animate("next", false);
+                }, options.pause);
+            };
+
+            if (options.numeric) setCurrent(0);
+
+            if (!options.continuous && options.controlsFade) {
+                $("a", "#" + options.prevId).hide();
+                $("a", "#" + options.firstId).hide();
+            };
+
+        });
+
+    };
+
+})(jQuery);
